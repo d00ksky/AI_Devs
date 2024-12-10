c31f3fdb164b76d65b4c538d5348edd52312e2cf
d00ksky <debski.jakub@gmail.com>
2024-12-10 03:18:51 +0100

    almost done with lesson20. Need to work on prompts. Maybe add some additional processing in another AI call

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,4 +1,5 @@
 import base64
+import json
 import os
 import requests
 from pdf2image import convert_from_path
@@ -13,7 +14,7 @@ load_dotenv()
 
 client = OpenAI()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY2")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 REPORT_URL = os.getenv("REPORT_URL")
 NOTES_URL = os.getenv("NOTES_URL")
 BASE_URL = os.getenv("BASE_URL")
@@ -76,15 +77,59 @@ def encode_image(image_path: str):
 def vision_transcription(image):
     base64_image = encode_image(image)
     response = client.chat.completions.create(
-        model="chatgpt-4o-latest",  # or your specific model
+        model="chatgpt-4o-latest",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant that transcribes images of notes into text. If text is in some color also describe the color and that it is distinct from other text."},
-            {"role": "user", "content": [
-                {"type": "text", "text": "Transcribe text from the image"},
-                {"type": "image_url", "image_url": f"data:image/jpeg;base64,{base64_image}"}
-            ]}
+            {
+                "role": "user", 
+                "content": [
+                    {"type": "text", "text": f"You are a helpful assistant that transcribes images of notes into text. If text is in some color also describe the color and that it is distinct from other text. If there is some image in the image, describe it.Transcribe text from the image and describe every picture and image. Here is the image: "},
+                    {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}
+                ]
+            }
         ]
     )
     return response.choices[0].message.content
 
+
+trancribed_images = []
+logging.info(f"Transcribing images from {output_dir}")
+for image in tqdm(os.listdir(output_dir), desc="Transcribing images"):
+    logging.info(f"Transcribing image {image}")
+    trancribed_images.append(vision_transcription(os.path.join(output_dir, image)))
+logging.info(f"Transcribed images: {trancribed_images}")
+
+
+def answer_questions(questions, trancribed_images):
+    logging.info(f"Answering questions: {questions}")
+    response = client.chat.completions.create(
+        model="o1-preview-2024-09-12",
+        messages=[
+            {"role": "user", "content": [{"type": "text", "text": f"You are a helpful assistant that answers questions based on the provided text. Answer in Polish. Answers should be concise and to the point. Provide answers in JSON format with keys as question numbers and values as answers with the following format: {{'01': 'answer1', '02': 'answer2', '03': 'answer3', '04': 'answer4', '05': 'answer5'}}. Don't add any other text to response, just the JSON. Uwzględnij wszystkie fakty podane w tekście, w szczególności odwołania do wydarzeń. Przeanalizuj dokładnie tekst, niektóre pytania są podchwytliwe. Answer the following questions  - {questions} . Answer based on the provided text: {trancribed_images}"}, ]}
+        ]
+    )
+    logging.info(f"Response: {response.choices[0].message.content}")
+    return response.choices[0].message.content
+
+logging.info("Answering questions")
+answer = answer_questions(questions, trancribed_images)
+logging.info(f"Answer: {answer}")
+answer = str(answer).replace("'", '"')
+answer = json.loads(answer)
+logging.info(f"Answer after processing: {answer}")
+
+def final_call(answer):
+    logging.info(f"Final call: {answer}")
+    json_data = {
+        "task": "notes",
+        "apikey": KLUCZ,
+        "answer": answer
+        
+    }
+    response = requests.post(f"{REPORT_URL}", json=json_data)
+    logging.info(f"Response: {response.json()}")
+    return response.json()
+
+solution = final_call(answer)
+
+print(solution)
 
